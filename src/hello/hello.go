4aeaa37c6e4ae365a1ce71e06326bae8e73fd1ad
join <wojoin98@gmail.com>
2019-02-19 03:56:32 -0500

    code organization

---
+++ src/hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/wojoin/go/stringutil"
+)
+
+func main() {
+    fmt.Printf("Hello, World\n")
+    fmt.Println(stringutil.Reverse("!oG ,olleH"))
+}
---
+++ src/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"github.com/wojoin/go/util"
+)
+
+
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	for pos, char := range "日本\x80語" { // \x80 is an illegal UTF-8 encoding
+		fmt.Printf("character %#U starts at byte position %d\n", char, pos)
+	}
+
+	for c := 'a'; c <= 'f'; c = c + 1 {
+		fmt.Print(util.Unhex(byte(c)))
+	}
+	fmt.Println("========")
+	for c := 'A'; c <= 'F'; c = c + 1 {
+		fmt.Print(util.Unhex(byte(c)))
+	}
+
+	fmt.Println("========")
+
+	util.DyncmicType(3)
+
+	log.Println("hello")
+
+	num, pos := util.NextInt([]byte("a123a456"), 0)
+	fmt.Println(num)
+	fmt.Println(pos)
+	
+	// defer statement is likely to stack, put statement into stack, it execute before the function executing the defer returns
+	// Deferred functions are executed in LIFO order
+	for i := 0; i < 5; i++ {
+		defer fmt.Printf("%d ", i) // output: 4 3 2 1 0
+	}
+
+}
+
+
